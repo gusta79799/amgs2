07f5dd6a66af4d834bdf51b9bdc69aa6e7e08fbc
Gustavo Silva <gs4043141@gmail.com>
2026-04-23 23:04:11 -0300

    Add files via upload

---
+++ ccx.js
@@ -0,0 +1,54 @@
+function login(){
+
+    var usuario = document.getElementById("usuario").value;
+    var senha = document.getElementById("senha").value;
+
+    if(usuario == "admin" && senha == "1234") {
+        document.getElementById("mensagem").style.color = "green";1234
+        document.getElementById("mensagem").innerText = "Login realizado com sucesso!";
+        
+        window.location.href = "https://drive.google.com/uc?export=view&id=17vOCfwC3kbP5o3m_0nDaAOMRPQb03tpM";
+    }
+ 
+    else {
+        document.getElementById("mensagem").style.color = "red";
+        document.getElementById("mensagem").innerText = "Usuário ou senha incorreta, tente novamente";
+    }
+
+}
+
+icon.addEventListener("click", () => {
+    const isPassword = senha.type == "password";
+
+    senha.type = isPassword ? "text" : "password";
+    icon.textContent = isPassword ? "👁️" : "👁️‍🗨️";
+});
+
+let player;
+let somLigado = false;
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('bg-video', {
+    events: {
+      'onReady': onPlayerReady
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  event.target.mute();
+}
+
+function toggleSom() {
+  if (!player) return;
+
+  if (somLigado) {
+    player.mute();
+    document.getElementById("btnSom").innerText = "🔇";
+  } else {
+    player.unMute();
+    document.getElementById("btnSom").innerText = "🔊";
+  }
+
+  somLigado = !somLigado;
+}
